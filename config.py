56378614f5562571986f093443e411d5adb97182
Connor Milliken <cmilliken09@gmail.com>
2019-02-21 09:44:30 -0500

    Added config change

--- config.py
+++ config.py
@@ -11,4 +11,6 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'app.db')
 
-    
+    # uri for postgres local database
+    # not for Heroku usage yet
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:po_admin@localhost:5434/ecommerce'
